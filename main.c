bef7b5080f51fb98baff20de9ea99c5cca701a98
yeonwoolee1 <144992849+yeonwoolee1@users.noreply.github.com>
2023-12-03 21:03:49 +0900

    실습1

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+struct student{
+	int ID;
+	char name[100];
+	float score;
+};
+int main(int argc, char *argv[]) {
+	struct student s1={123,"Juyeop",4.3};
+	
+	s1.ID=123456;
+	s1.name[0]='c';
+	s1.score=0.7;	
+	
+	printf("ID: %i\n", s1.ID);
+	printf("name: %s\n", s1.name);
+	printf("score: %f\n", s1.score);
+	
+	strcpy(s1.name,"Bongjun");
+	printf("name2: %s\n");
+	
+	return 0;
+}
+
+
